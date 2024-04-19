4ab195ad0d68ff60c3d5839214a446e80d0a10d0
PozyalovMihail <pozyalov@gmail.com>
2024-04-19 22:56:46 +0300

    добавил SplashScreen

--- presentation/src/main/java/com/example/presentation/shopping_cart_screen/ShoppingCartScreen.kt
+++ presentation/src/main/java/com/example/presentation/shopping_cart_screen/ShoppingCartScreen.kt
@@ -1,7 +1,6 @@
 package com.example.presentation.shopping_cart_screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
